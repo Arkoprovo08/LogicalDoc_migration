2125596ff1f5af09897afa1d5fca89e9c1140e43
unknown <arkoprovoghosh17@gmail.com>
2025-05-30 15:56:46 +0530

    third commit

--- fileUpload.py
+++ fileUpload.py
@@ -1,18 +1,19 @@
 import os
 import requests
-import psycopg2
+import oracledb
 from datetime import datetime
 
 DOCUMENT_FOLDER = r'C:\Users\Administrator.DGH\Desktop\dgh\Files'
 API_URL = "http://k8s-ingressn-ingressn-1628ed6eec-bd2bc8d22bd4aed8.elb.ap-south-1.amazonaws.com/documentManagement/uploadDocument"
 
-conn = psycopg2.connect(
-    dbname='orcl',
+# Oracle DB connection using oracledb in thin mode
+conn = oracledb.connect(
     user='sys',
     password='Dgh1234',
-    host='192.168.0.133',
-    port='1521'
+    dsn='192.168.0.133:1521/orcl',
+    mode=oracledb.SYSDBA
 )
+
 cursor = conn.cursor()
 
 process_name = "Appointment of Auditor"
@@ -49,7 +50,7 @@ for file_name in os.listdir(DOCUMENT_FOLDER):
                     cursor.execute("""
                         INSERT INTO global_master.t_document_migration_details 
                         (document_name, logical_doc_id, is_migrated, created_date, created_by)
-                        VALUES (%s, %s, %s, %s, %s)
+                        VALUES (:1, :2, :3, :4, :5)
                     """, (
                         file_name, doc_id, 1, datetime.now(), created_by
                     ))
@@ -57,7 +58,7 @@ for file_name in os.listdir(DOCUMENT_FOLDER):
                     cursor.execute("""
                         INSERT INTO global_master.t_doc_migration_error_logs 
                         (doc_name, error_message, timestamp)
-                        VALUES (%s, %s, %s)
+                        VALUES (:1, :2, :3)
                     """, (
                         file_name, response.text, datetime.now()
                     ))
@@ -66,7 +67,7 @@ for file_name in os.listdir(DOCUMENT_FOLDER):
             cursor.execute("""
                 INSERT INTO global_master.t_doc_migration_error_logs 
                 (doc_name, error_message, timestamp)
-                VALUES (%s, %s, %s)
+                VALUES (:1, :2, :3)
             """, (
                 file_name, str(e), datetime.now()
             ))
